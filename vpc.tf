e3aa8d4d69e15f12db081892f62c8457df070474
lathifashaik <shaiklathi02@gmail.com>
2023-12-08 09:54:59 +0000

    create pub subnet - A

--- vpc.tf
+++ vpc.tf
@@ -8,5 +8,17 @@ resource "aws_vpc" "tf-ecomm" {
   }
 }
 
+# Create pub subnet - az - A
+resource "aws_subnet" "tf-ecomm-pub-sn-A" {
+  vpc_id     = aws_vpc.tf-ecomm.id
+  cidr_block = "192.168.0.0/16"
+availability_zone = "us-east-2a"
+map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "ecomm-public-subnet-A"
+  }
+}
+
 
 
